6577e633293072bc1d886e5d9e412af1a2c00e7f
Natasha S Love <debora@inbox.ru>
2018-01-24 20:04:48 -0600

    if else

--- examples.cpp
+++ examples.cpp
@@ -5,6 +5,25 @@ int main () {
 int room_width = 5, room_length = 10;
 float room_radius = 10.0;
 float room_circle_area = (room_radius*room_radius)*3.14;
+bool this_is_always_true = true;
+
+cout << "Enter your first number: ";
+cin >> room_length;
+cout << "Enter your second number: ";
+cin >> room_width;
+
+
+if(room_length == room_width) {
+	cout << "Length and Width are the same" << endl;
+} else {
+cout << "Length and Width are not the same" << endl;
+}
+if (room_length > room_width) {
+	cout << "length is greater than width";
+} else {
+	cout << "width is greater than length" << endl;
+}
+
 
 //Printing out
 cout << " Our first number is " << room_width << endl;
