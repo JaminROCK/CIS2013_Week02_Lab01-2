7c5e013fecdefc426d4483de130386b05368465d
Natasha S Love <debora@inbox.ru>
2018-01-24 19:45:53 -0600

    corrected

--- examples.cpp
+++ examples.cpp
@@ -7,13 +7,13 @@ float room_radius = 10.0;
 float room_circle_area = (room_radius*room_radius)*3.14;
 
 //Printing out
-cout << "Our first number is " << room_width << endl;
+cout << " Our first number is " << room_width << endl;
 cout << " Our second number is " << room_length << endl;
 // Different math operations
 // Finding the room area
-cout << " If you add them you get " << room_width+room_length; "\n";
+cout << " If you multiply them you get room area " << room_width*room_length << endl;
 
-cout << " Unless your room is a circle " << room_circle_area << endl;
+cout << " Unless your room is a circle, then it is " << room_circle_area << endl;
 
 return 0;
 
